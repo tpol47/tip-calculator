5bf089da8ede74db699f496a85171ac0c9a71718
Thomas Pollick <thomascpollick@gmail.com>
2023-09-29 13:22:52 -0500

    create new files

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculate15Tip () {
+    const totalAmount = parseFloat(document.getElementById('tip-input').value)
+    const tipAmount = totalAmount * .15
+    const result = document.getElementById('result')
+    result.innerText = tipAmount
+}
+
+function calculate10Tip () {
+    const totalAmount = parseFloat(document.getElementById('tip-input').value)
+    const tipAmount = totalAmount * .10
+    const result = document.getElementById('result')
+    result.innerText = tipAmount
+}
+
+function calculate20Tip () {
+    const totalAmount = parseFloat(document.getElementById('tip-input').value)
+    const tipAmount = totalAmount * .20
+    const result = document.getElementById('result')
+    result.innerText = tipAmount
+}
